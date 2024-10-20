ee030e53859ae716e7d77570fcf99d7bc1382cbb
Neel <gourav.4871@gmail.com>
2024-10-20 21:41:17 +0200

    faster compute

--- decomp.py
+++ decomp.py
@@ -10,19 +10,20 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 
-def get_scheduling_delay(send_ts, sched_arr, slots_per_frame=20, slots_duration_ms=0.5):
-    for j in range(0, len(sched_arr)):
-        if sched_arr[j]['schedule_ts']>send_ts and sched_arr[j]['schedule_ts']-send_ts<slots_per_frame*slots_duration_ms:
-            return (sched_arr[j]['schedule_ts']-send_ts)*1000
-    return None
+def get_scheduling_delay(packet, sched_sorted_list, slots_per_frame=20, slots_duration_ms=0.5):
+    idx=sched_sorted_list.bisect_right(packet['ip.in_t'])
+    if idx < len(sched_sorted_list):
+        return (sched_sorted_list[idx]-packet['ip.in_t'])*1000
+    else:
+        return None
 
-def get_frame_alignment_delay(send_ts, bsrupd_arr, slots_per_frame=20, slots_duration_ms=0.5):
-    for j in range(0, len(bsrupd_arr)):
-        if bsrupd_arr[j]['timestamp']>send_ts and bsrupd_arr[j]['timestamp']-send_ts<slots_per_frame*slots_duration_ms:
-            return (bsrupd_arr[j]['timestamp']-send_ts)*1000
-    return None
+def get_frame_alignment_delay(packet, sr_bsr_tx_sorted_list, slots_per_frame=20, slots_duration_ms=0.5):
+    idx=sr_bsr_tx_sorted_list.bisect_right(packet['ip.in_t'])
+    if idx < len(sr_bsr_tx_sorted_list):
+        return (sr_bsr_tx_sorted_list[idx]-packet['ip.in_t'])*1000
+    else:
+        return None
     
-# return queueing delay in millisecons
 def get_queueing_delay(packet):
     min_delay = np.inf
     for rlc_seg in packet['rlc.attempts']:
@@ -41,14 +42,26 @@ def get_ran_delay(packet):
         logger.error(f"Packet {packet['id']} either ip.in_t or ip.out_t not present")
         return None
 
+def get_ran_delay_wo_frame_alignment_delay(packet, sr_bsr_tx_sorted_list, slots_per_frame=20, slots_duration_ms=0.5):
+    if get_ran_delay(packet)!=None and get_frame_alignment_delay(packet, sr_bsr_tx_sorted_list, slots_per_frame=20, slots_duration_ms=0.5)!=None:
+        return get_ran_delay(packet)-get_frame_alignment_delay(packet, sr_bsr_tx_sorted_list, slots_per_frame=20, slots_duration_ms=0.5)
+    else:
+        return None
+
+def get_ran_delay_wo_scheduling_delay(packet, sched_sorted_list, slots_per_frame=20, slots_duration_ms=0.5):
+    if get_ran_delay(packet)!=None and get_scheduling_delay(packet, sched_sorted_list, slots_per_frame=20, slots_duration_ms=0.5)!=None:
+        return get_ran_delay(packet)-get_scheduling_delay(packet, sched_sorted_list, slots_per_frame=20, slots_duration_ms=0.5)
+    else:
+        return None
+
 def get_retx_delay_seg(packet, rlc_seg):
     max_delay, min_delay = 0, np.inf
     for mac_attempt in  rlc_seg['mac.attempts']:
-        if mac_attempt.get('phy.out_t')!=None:
-            max_delay = max(max_delay, mac_attempt['phy.out_t'])
-            min_delay = min(min_delay, mac_attempt['phy.out_t'])
+        if mac_attempt.get('phy.in_t')!=None:
+            max_delay = max(max_delay, mac_attempt['phy.in_t'])
+            min_delay = min(min_delay, mac_attempt['phy.in_t'])
         else:
-            logger.error(f"Packet {packet['id']} phy.out_t not present")
+            logger.error(f"Packet {packet['id']} phy.in_t not present")
             return None
     return (max_delay-min_delay)*1000
 
